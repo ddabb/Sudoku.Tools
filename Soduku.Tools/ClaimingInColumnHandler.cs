fcd77ff13f3e766ce91649fc5b10c5ea5828b9f2
ddabb <417144452@qq.com>
2019-12-31 18:49:34 +0800

    ClaimingInColumnHandler done

--- Soduku.Tools/ClaimingInColumnHandler.cs
+++ Soduku.Tools/ClaimingInColumnHandler.cs
@@ -16,35 +16,68 @@ namespace Sudoku.Tools
             List<CellInfo> cells = new List<CellInfo>();
             Func<CellInfo, bool> predicate = c => c.Value == 0;
             var rests = qSoduku.GetFilterCell(predicate).OrderBy(c => c.Column).ThenBy(c => c.Block).ToList();
-            foreach (var item in rests)
-            {
-                Debug.WriteLine(typeof(ClaimingInColumnHandler) + " 位置  " + item.index + "  行号  " + item.Row +
-                                "  列号  " + item.Column + " 宫 " + item.Block + " 候选数 " +
-                                string.Join(",", qSoduku.GetRest(item.index)));
-            }
-
             var columnBlockDtos = rests.Select(c => new {c.Column, c.Block}).Distinct().ToList();
             List<ColumnBlockDto> allDtos = new List<ColumnBlockDto>();
             foreach (var dto in columnBlockDtos)
             {
-                ColumnBlockDto temp = new ColumnBlockDto();
-                temp.Block = dto.Block;
-                temp.Column = dto.Column;
+                ColumnBlockDto temp = new ColumnBlockDto {Block = dto.Block, Column = dto.Column};
                 var filter = rests.Where(c => c.Block == dto.Block && c.Column == dto.Column);
                 foreach (var filterItem in filter)
                 {
                     temp.rests.AddRange(qSoduku.GetRest(filterItem.index));
                 }
+
                 temp.rests = temp.rests.Distinct().ToList();
                 allDtos.Add(temp);
             }
 
-            foreach (var eachDto in allDtos)
+
+
+            var columns = allDtos.Select(c => c.Column).Distinct().ToList();
+            Dictionary<int, List<int>> columnmap = new Dictionary<int, List<int>>();
+            foreach (var eColumnindex in columns)
             {
-                Debug.WriteLine("eachDto  列 " + eachDto.Column + "宮" + eachDto.Block + "候选数" +
-                                string.Join(",", eachDto.rests));
+                List<int> allRestInt = new List<int>();
+                var allrest = allDtos.Where(c => c.Column == eColumnindex).ToList();
+                foreach (var eachRest in allrest)
+                {
+                    allRestInt.AddRange(eachRest.rests);
+                }
+
+                columnmap.Add(eColumnindex, allRestInt);
+
             }
 
+            List<ColumnBlockSingle> list = new List<ColumnBlockSingle>();
+
+            foreach (var kv in columnmap)
+            {
+                foreach (var groupItem in kv.Value.GroupBy(c => c).Distinct().ToList())
+                {
+                    if (groupItem.Count() == 1)
+                    {
+                        list.Add(new ColumnBlockSingle
+                        {
+                            Column = kv.Key, Value = groupItem.Key,
+                            Block = allDtos.First(c => c.Column == kv.Key && c.rests.Contains(groupItem.Key)).Block
+                        });
+
+                    }
+                }
+            }
+            //Debug.WriteLine("候选数" + item.Value + " 在列 " + item.Column + "在宮" + item.Block + "只出现了一次");
+            foreach (var item in list)
+            {
+                foreach (var item1 in rests.Where(c => c.Block == item.Block && c.Column != item.Column))
+                {
+                    var cellrest = qSoduku.GetRest(item1.index);
+                    if (cellrest.Count == 2 && cellrest.Contains(item.Value))
+                    {
+                        item1.Value = cellrest.First(c => c != item.Value);
+                        cells.Add(item1);
+                    }
+                }
+            }
             return cells;
         }
 
@@ -53,6 +86,13 @@ namespace Sudoku.Tools
             public int Column;
             public int Block;
             public List<int> rests = new List<int>();
+        }
+
+        public class ColumnBlockSingle
+        {
+            public int Column;
+            public int Block;
+            public int Value;
         }
     }
 }
