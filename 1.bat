975bb7f42b7266e45a7855b54f74486088d6820b
ddabb <417144452@qq.com>
2020-01-02 11:16:18 +0800

    'updategitignore'

--- 1.bat
+++ 1.bat
@@ -1,14 +1,11 @@
 @echo off
 cd E\ E:\Git\Sudoku.Tools
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Console.deps.json
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Console.dll
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Console.pdb
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Console.runtimeconfig.dev.json
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Console.runtimeconfig.json
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Core.dll
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Core.pdb
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Tools.dll
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\Sudoku.Tools.pdb
+git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\*
+git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Test\bin\Debug\netcoreapp2.1\*
+git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Test\obj\Debug\netcoreapp2.1\*
+git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Tools\bin\Debug\netcoreapp2.1\*
+git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Tools\obj\Debug\netcoreapp2.1\*
 git add .
 git commit -m 'updategitignore'
 cmd
+
