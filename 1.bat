420618c39c0e0871e2c125358be56a182ce7a1e7
ddabb <417144452@qq.com>
2020-01-02 11:18:07 +0800

    'updategitignore'

--- 1.bat
+++ 1.bat
@@ -1,9 +1,5 @@
 @echo off
 cd E\ E:\Git\Sudoku.Tools
-git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Console\obj\Debug\netcoreapp2.1\*
-git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Test\bin\Debug\netcoreapp2.1\*
-git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Test\obj\Debug\netcoreapp2.1\*
-git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Tools\bin\Debug\netcoreapp2.1\*
 git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Tools\obj\Debug\netcoreapp2.1\*
 git add .
 git commit -m 'updategitignore'
