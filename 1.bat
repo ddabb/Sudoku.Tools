e59ace7cae7c1c14891d4cc066d943b815d8cea1
ddabb <417144452@qq.com>
2020-01-02 11:20:51 +0800

    'updategitignore'

--- 1.bat
+++ 1.bat
@@ -1,6 +1,7 @@
 @echo off
 cd E\ E:\Git\Sudoku.Tools
-git rm -r --cached  E:\Git\Sudoku.Tools\SudoKu.Tools\obj\Debug\netcoreapp2.1\*
+git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Tools\obj\Debug\netcoreapp2.1\Sudoku.Tools.assets.cache
+git rm -r --cached  E:\Git\Sudoku.Tools\Sudoku.Tools\obj\Debug\netcoreapp2.1\Sudoku.Tools.csproj.FileListAbsolute.txt
 git add .
 git commit -m 'updategitignore'
 cmd
