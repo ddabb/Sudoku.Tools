ff6b94d9749a43f55935cf58c8fe534fe3cfde9c
ddabb <417144452@qq.com>
2020-01-05 20:22:27 +0800

    补充单元格关系，寻找链

--- Sudoku.Core/NegativeCellInfo.cs
+++ Sudoku.Core/NegativeCellInfo.cs
@@ -6,9 +6,17 @@ namespace Sudoku.Core
 {
     public class NegativeCellInfo : CellInfo
     {
+        /// <summary>
+        /// 前置肯定单元格
+        /// </summary>
+        public PositiveCellInfo reductionBeforeCell;
+        /// <summary>
+        /// 后置肯定单元格链表
+        /// </summary>
+        public List<PositiveCellInfo> reductionAfterCells;
         public NegativeCellInfo(int index, int value) : base(index, value)
         {
-            
+
         }
     }
 }
--- Sudoku.Core/PositiveCellInfo.cs
+++ Sudoku.Core/PositiveCellInfo.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace Sudoku.Core
 {
- public   class PositiveCellInfo : CellInfo
+    public class PositiveCellInfo : CellInfo
     {
+        /// <summary>
+        /// 前置否定单元格
+        /// </summary>
+        public NegativeCellInfo reductionBeforeCell;
+        /// <summary>
+        /// 后置否定单元格链表
+        /// </summary>
+        public List<NegativeCellInfo> reductionAfterCells;
         public PositiveCellInfo(int index, int value) : base(index, value)
         {
+
 
         }
     }
