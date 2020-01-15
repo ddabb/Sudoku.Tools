d9d519d8ec13641199684f2c34479a12bd2a83ab
ddabb <417144452@qq.com>
2020-01-15 14:24:28 +0800

    下一版本计算边距

--- Sudoku.UI/Form1.Designer.cs
+++ Sudoku.UI/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sudokuPanel1 = new Sudoku.UI.SudokuPanel();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.editMenuItem,
+            this.toolMenuItem,
+            this.optionMenuItem,
+            this.helpMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1173, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.fileMenuItem.Text = "文件";
+            // 
+            // editMenuItem
+            // 
+            this.editMenuItem.Name = "editMenuItem";
+            this.editMenuItem.Size = new System.Drawing.Size(48, 21);
+            this.editMenuItem.Text = " 编辑";
+            // 
+            // toolMenuItem
+            // 
+            this.toolMenuItem.Name = "toolMenuItem";
+            this.toolMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.toolMenuItem.Text = "工具";
+            // 
+            // optionMenuItem
+            // 
+            this.optionMenuItem.Name = "optionMenuItem";
+            this.optionMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.optionMenuItem.Text = "选项";
+            // 
+            // helpMenuItem
+            // 
+            this.helpMenuItem.Name = "helpMenuItem";
+            this.helpMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.helpMenuItem.Text = "帮助";
             // 
             // sudokuPanel1
             // 
             this.sudokuPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(215)))), ((int)(((byte)(215)))));
-            this.sudokuPanel1.Location = new System.Drawing.Point(12, 12);
+            this.sudokuPanel1.Location = new System.Drawing.Point(12, 53);
             this.sudokuPanel1.Name = "sudokuPanel1";
             this.sudokuPanel1.Size = new System.Drawing.Size(810, 810);
             this.sudokuPanel1.TabIndex = 0;
@@ -45,16 +96,27 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1147, 853);
+            this.ClientSize = new System.Drawing.Size(1173, 887);
             this.Controls.Add(this.sudokuPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private SudokuPanel sudokuPanel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpMenuItem;
     }
 }
