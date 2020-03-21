bc293c212ead5e268b7cbca93478c9a6b08a673f
ddabb <417144452@qq.com>
2020-03-21 18:39:56 +0800

    调整顺序

--- Sudoku.Core/SolveMethodEnum.cs
+++ Sudoku.Core/SolveMethodEnum.cs
@@ -27,6 +27,8 @@ namespace Sudoku.Core
         [Description("列区块")]
         [DifficultuRating(2.8)]
         ClaimingInColumn,
+        [Description("级联区块")]
+        CascadingLockedCandidates,
         [Description("隐性数对")]
         [DifficultuRating(2.0)]
         HiddenPair,
@@ -45,9 +47,16 @@ namespace Sudoku.Core
         [Description("显性四数组")]
         [DifficultuRating(5.0)]
         NakedQuadruple,
+        [Description("区块数组")]
+        NakedSubsetWithLockedCandidates,
+        [Description("隔一数对匹配法")]
+        MWing,
         [DifficultuRating(4.4)]
         [Description("首尾数对匹配法(WWing)")]
         WWing,
+        FinnedXwing,
+        SashimiXwing,
+        SiameseXwing,
         [DifficultuRating(3.2)]
         XWing,
         [DifficultuRating(4.2)]
@@ -56,43 +65,24 @@ namespace Sudoku.Core
         XYZWing,
         [DifficultuRating(4.6)]
         WXYZWing,
-        [Description("双值格链")]        
+        VWXYZWing,
+        [Description("拐角匹配法")]
+        LocalWing,
+        [Description("杂合匹配法")]
+        HybridWing,
+        [Description("分裂匹配法")]
+        SplitWing,
+        [DifficultuRating(4.6)]
+        IncompleteVWXYZWing,
+        [DifficultuRating(4.5)]
+        IncompleteWXYZWing,
+        [Description("双值格链")]
         XYChain,
         [DifficultuRating(6.6)]
         ForcingXChain,
         URType1,
-        [DifficultuRating(4.6)]
-        ULSize6Type1,
-        ULSize6Type2,
-        [Description("双线风筝")]
-        [DifficultuRating(4.1)]
-        TwoStringsKite,
-        [Description("摩天楼")]
-        [DifficultuRating(4.0)]
-        Skyscraper,
-        [Description("多宝鱼(普通)")]
-        [DifficultuRating(4.2)]
-        TurbotFishNormal,
-        [Description("隔一数对匹配法")]
-        MWing,
-        Jellyfish,
-        HiddenAR,
-        LockedSubset,
-        [DifficultuRating(4.7)]
-        ULSize6Type3,
-        ImcompletedURType2,
-        ALPExtendedType,
-        SashimiSwordfish,
-        XRSize6Type4,
         [DifficultuRating(4.5)]
         URType2,
-        ARType3WithHiddenTriple,
-        ALPBasicType,
-        ALTBasicType,
-        ALTExtendedType,
-        ARType2,
-        ARType1,
-        XRSize10,
         [Description("唯一矩形+显性数对")]
         [DifficultuRating(4.5)]
         URType3NakedPair,
@@ -110,74 +100,86 @@ namespace Sudoku.Core
         URType3HiddenQuadruple,
         [DifficultuRating(4.5)]
         URType4,
-        VWXYZWing,
-        XRSize6Type3,
-        XRSize8,
-        XRSize6Type1,
-        XRSize6Type2,
-        XRSize12,
-        XRSize14,
-        ARType3WithNakedPair,
-        BugType1,
-        [DifficultuRating(5.2)]
-        FinnedJeffyfish,
-        [DifficultuRating(3.8)]
-        FinnedSwordfish,
-        [DifficultuRating(3.8)]
-        Swordfish,
-        FinnedXwing,
         ImcompletedURType1,
+        ImcompletedURType2,
         ImcompletedURType3,
-        LockedCandidates,
+        ImcompletedURType4,
         LockedURType1,
         LockedURType2,
-        SiameseSwordfish,
-        SiameseXwing,
-        [DifficultuRating(5.0)]
-        ULSize10,
+        [DifficultuRating(4.6)]
+        ULSize6Type1,
+        ULSize6Type2,
         [DifficultuRating(4.7)]
-        ULSize8,
+        ULSize6Type3,
         [DifficultuRating(4.6)]
         ULSize6Type4,
-        ULSize14,
-        RemotePair,
-        SashimiJellyfish,
-        SashimiXwing,
-        SiameseJeffyfish,
+        [DifficultuRating(4.7)]
+        ULSize8,
+        [DifficultuRating(5.0)]
+        ULSize10,
         ULSize12,
-        [Description("拐角匹配法")]
-        LocalWing,
-        [Description("杂合匹配法")]
-        HybridWing,
-        [Description("分裂匹配法")]
-        SplitWing,
-        [DifficultuRating(4.6)]
-        IncompleteVWXYZWing,
+        ULSize14,
+        [Description("双线风筝")]
+        [DifficultuRating(4.1)]
+        TwoStringsKite,
+        [Description("摩天楼")]
+        [DifficultuRating(4.0)]
+        Skyscraper,
+        [Description("多宝鱼(普通)")]
+        [DifficultuRating(4.2)]
+        TurbotFishNormal,
+        [Description("远程数对")]
+        RemotePair,
+        [DifficultuRating(3.8)]
+        FinnedSwordfish,
+        SashimiSwordfish,
+        SiameseSwordfish,
+        [DifficultuRating(3.8)]
+        Swordfish,
+        SashimiJellyfish,
+        SiameseJeffyfish,
+        [DifficultuRating(5.2)]
+        FinnedJeffyfish,
+        Jellyfish,
+        HiddenAR,
+        LockedSubset,
+        ALPBasicType,
+        ALPExtendedType,
+        ALTBasicType,
+        ALTExtendedType,
+        ARType2,
+        ARType1,
+        ARType3WithNakedPair,
+        ARType3WithHiddenTriple,
+        XRSize6Type1,
+        XRSize6Type2,
+        XRSize6Type3,
+        XRSize6Type4,
+        XRSize8,
+        XRSize10,
+        XRSize12,
+        XRSize14,
+        BugType1,
+        LockedCandidates,
         [Description("空矩形")]
         EmptyRectangle,
         [Description("不连续环")]
         DiscontinuousNiceLoop,
         CannibalisticAIC,
-        [Description("均衡三数组")]
-        AlignedTripleExclusion,
         [Description("均衡数对")]
         AlignedPairExclusion,
+        [Description("均衡三数组")]
+        AlignedTripleExclusion,
         [Description("均衡四数组")]
         AlignedQuadrupleExclusion,
-        [Description("级联区块")]
-        CascadingLockedCandidates,
-        [Description("区块数组")]
-        NakedSubsetWithLockedCandidates,
         [Description("强制链(开)")]
         [DifficultuRating(7.3)]
         ForcingChainOn,
         [Description("强制链(关)")]
         ForcingChainOff,
-        ImcompletedURType4,
         [DifficultuRating(9.1)]
         DymanicForcingChain,
-        [DifficultuRating(4.5)]
-        IncompleteWXYZWing,
+
 
     }
 }
